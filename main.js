4b02453b83f3ead391574cd7a07e9e335672db50
风雨兼程 <xiuxian>
2024-07-21 18:10:38 +0800

    feat: 3-2 功能流程和文件结构init simple-music-player

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    // 为了访问渲染器中的Node.js的某些功能，我们在 BrowserWindow 的构造函数上附加了一个预加载脚本。
+  });
+
+  // 加载 index.html
+  mainWindow.loadFile("index.html");
+
+  // 打开开发工具
+  // mainWindow.webContents.openDevTools()
+};
+
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(() => {
+  // 打开您的窗口，Electron 中，只有在 app 模块的 ready 事件被激发后才能创建浏览器窗口
+  createWindow();
+
+  // 如果没有窗口打开则打开一个窗口 (macOS)
+  app.on("activate", () => {
+    // 在 macOS 系统内, 如果没有已开启的应用窗口
+    // 点击托盘图标时通常会重新创建一个新窗口
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
+// 对应用程序和它们的菜单栏来说应该时刻保持激活状态,
+// 直到用户使用 Cmd + Q 明确退出
+app.on("window-all-closed", () => {
+  // 关闭所有窗口时退出应用 (Windows & Linux)
+  if (process.platform !== "darwin") app.quit();
+});
+
+// 在当前文件中你可以引入所有的主进程代码
+// 也可以拆分成几个文件，然后用 require 导入。
